b00976c1640fd2bb881a5f9004ac7fbe37aaaa9c
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-03-17 12:46:53 -0300

    syncing

--- sendFilesToFTP.js
+++ sendFilesToFTP.js
@@ -3,7 +3,9 @@ var Client = require('ftp');
 var dir = require('node-dir');
 
 var data=[];
+var fileNames = [];
 var c = new Client();
+var counter = 0;
 
 dir.readFiles('/Users/fcavalcanti/Desktop/betapostcrawler/', {
     match: /.txt$/,
@@ -15,16 +17,18 @@ dir.readFiles('/Users/fcavalcanti/Desktop/betapostcrawler/', {
     },
     function(err, files){
         if (err) throw err;
-        console.log('finished reading files:',files);
+        console.log('finished reading files: ',files.length);
+        fileNames = files;
     });
 
   c.on('ready', function() {
     data.forEach(function(entry) {
-        console.log(entry);
-        c.put(entry, '/public_html/projeto_mdp_des/teste/'+entry, function(err) {
+      console.log(fileNames[counter].split("/")[5]);
+        c.put(entry, '/public_html/projeto_mdp_des/teste/'+fileNames[counter].split("/")[5], function(err) {
           if (err) throw err;
           c.end();
         });
+        counter = counter +1;
     });
   });
   c.connect({ host: 'HOST', user: 'USER', password: 'PASS' });
